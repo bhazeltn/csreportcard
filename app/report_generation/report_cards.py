e44254afc4ab62ff0cb801e4be94802554107369
bhazeltn <bradley@thehazeltons.ca>
2024-04-19 17:56:22 -0600

    Invidual report cards merge to one pdf

--- app/report_generation/report_cards.py
+++ app/report_generation/report_cards.py
@@ -42,6 +42,29 @@ def generate_individual_report_card(evaluation_data, achievements_df, template_p
             update_achievements(annotations, achievements_df, evaluation_data['Skater Name'])
     PdfWriter(output_path, trailer=template_pdf).write()  # Save the filled PDF
 
+def merge_report_cards(directory_path, output_path):
+    """
+    Merge all PDF files in a specified directory into a single PDF file.
+    
+    Args:
+        directory_path (str): Path to the directory containing the PDF files.
+        output_path (str): Path to save the merged PDF file.
+    """
+    writer = PdfWriter()
+    files = [f for f in os.listdir(directory_path) if f.endswith('.pdf')]
+    files.sort()  # Optional: sort files if needed
+
+    for filename in files:
+        path = os.path.join(directory_path, filename)
+        reader = PdfReader(path)
+        writer.addpages(reader.pages)
+        print(f"Added {filename}")
+
+    writer.write(output_path)
+    print(f"Merged PDF saved as {output_path}")
+
+
+
 def map_field_name(field_name):
     """Map the field name from the PDF to the column names used in the dataframes."""
     return field_name.replace('_', ' ')
@@ -60,3 +83,7 @@ def create_report_cards(evaluations_df, achievements_df, template_path, output_d
         output_path = os.path.join(individual_directory, f"{skater_name.replace(' ', '_')}_Report_Card.pdf")
         generate_individual_report_card(evaluation_data, achievements_df, template_path, output_path)
         print(f"Report card generated for {skater_name} at {output_path}")
+    
+    merged_path = os.path.join(output_directory, 'report_cards.pdf')
+    
+    merge_report_cards(individual_directory, merged_path)
